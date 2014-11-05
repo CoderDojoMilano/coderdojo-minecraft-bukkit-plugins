81354d5d4e8cf4c540c9842ebf757b5966587268
antonio castaldi <toto.castaldi@gmail.com>
2014-11-05 12:41:29 +0100

    build a cube. Interface for commands

--- src/main/java/it/toto/minecraft/plugin/ShowCase.java
+++ src/main/java/it/toto/minecraft/plugin/ShowCase.java
@@ -1,8 +1,11 @@
 package it.toto.minecraft.plugin;
 
 
+import com.google.common.base.CaseFormat;
+import it.toto.minecraft.plugin.command.BuildACube;
 import it.toto.minecraft.plugin.util.DebugLog;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,17 +33,30 @@ public class ShowCase extends JavaPlugin {
             String commandLabel,
             String[] args
     ) {
-        log.info("sender {}", sender); //GlowPlayer vs net.glowstone.ConsoleManager$ColoredCommandSender
-        DebugLog.of(log).debug("command {}", command); //org.bukkit.command.PluginCommand
-        DebugLog.of(log).debug("commandLabel {}", commandLabel);
-        DebugLog.of(log).debug("args {}", args);
+        final DebugLog debugLog = DebugLog.of(log);
 
-        if (commandLabel.equalsIgnoreCase("hello")) {
-            String msg = "[Server] ";
-            getServer().broadcastMessage(msg);
-            return true;
+        debugLog.debug("sender {}", sender); //GlowPlayer vs net.glowstone.ConsoleManager$ColoredCommandSender
+        debugLog.debug("command {}", command); //org.bukkit.command.PluginCommand
+        debugLog.debug("commandLabel {}", commandLabel);
+        debugLog.debug("args {}", args);
+
+        final String commandClassName = BuildACube.class.getPackage().getName() + "." + CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_CAMEL, commandLabel);
+
+        log.info("execute command with class name {}", commandClassName);
+
+        CommandExecution commandExecution = null;
+
+        try {
+            commandExecution = (CommandExecution) Class.forName(commandClassName).newInstance();
+        } catch (Exception e) {
+            log.error("can't build command execution class ",e);
         }
-        return false;
+
+        if (commandExecution != null) {
+            return commandExecution.go(sender, command, args);
+        } else {
+            return false;
+        }
     }
 }
 
---
+++ src/main/java/it/toto/minecraft/plugin/command/BuildACube.java
@@ -0,0 +1,117 @@
+package it.toto.minecraft.plugin.command;
+
+import com.google.common.collect.Iterables;
+import it.toto.minecraft.plugin.CommandExecution;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.NumberUtils;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by toto on 05/11/14.
+ */
+@Slf4j
+public class BuildACube implements CommandExecution {
+
+
+    Location origin = null;
+    boolean firstHouse = true;
+
+    // Create a 3D cube, offset from the saved "origin"
+    void makeCube(int offsetX, int offsetY, int offsetZ, int width, int height, Material what) {
+        int i, j, k;
+        Location loc = new Location(origin.getWorld(), 0,0,0);
+
+        // Base is i X j, k goes up for height
+        for (i=0; i< width; i++) {
+            for (j=0; j<width; j++) {
+                for (k=0; k< height; k++) {
+                    loc.setX(origin.getX() + offsetX + i);
+                    loc.setZ(origin.getZ() + offsetZ + j);
+                    loc.setY(origin.getY() + offsetY + k);
+                    origin.getWorld().getBlockAt(loc).setType(what);
+                }
+            }
+        }
+    }
+
+    // Called by reader's code
+    // Minimum of 5 X 5
+    @SuppressWarnings("deprecation")
+    void buildMyHouse(int width, int height) {
+
+        // Floor the dimensions at no less than 5 X 5
+        if (width < 5) {
+            width = 5;
+        }
+        if (height < 5) {
+            height = 5;
+        }
+
+        if (firstHouse) {
+            // Center the first house on the player
+            origin.setY(origin.getY() - 1);
+            origin.setZ(origin.getZ() - (width/2));
+            origin.setX(origin.getX() - (width/2));
+            firstHouse = false;
+        }
+
+        // Set the whole area to wood
+        makeCube(0,0,0,width, height, Material.WOOD);
+        // Set the inside of the cube to air
+        makeCube(1,1,1,width-2, height-2, Material.AIR);
+
+        // Pop a door in one wall
+        Location door = new Location(origin.getWorld(),
+                origin.getX()+(width/2), origin.getY(), origin.getZ());
+
+        // The door is two high, with a torch over the door
+        door.setY(door.getY() +1);
+        Block bottom = origin.getWorld().getBlockAt(door);
+        door.setY(door.getY() +1);
+        Block top = origin.getWorld().getBlockAt(door);
+        door.setY(door.getY() +1);
+        door.setZ(door.getZ() +1);
+        Block over = origin.getWorld().getBlockAt(door);
+
+        // Magic values to establish top and bottom of door
+        top.setData((byte)0x8);
+        bottom.setData((byte)0x4);
+        // And normal material constants
+        top.setType(Material.WOODEN_DOOR);
+        bottom.setType(Material.WOODEN_DOOR);
+        over.setType(Material.TORCH);
+
+        // Move over to make next house if called in a loop.
+        origin.setX(origin.getX() + width);
+    }
+
+    @Override
+    public boolean go(CommandSender sender, Command command, String[] args) {
+        if (sender instanceof Player) {
+            Player me = (Player) sender;
+            // Put your code after this line:
+            origin = me.getLocation();
+            firstHouse = true;
+
+            int width = 6;
+            int height = 6;
+            if (args.length > 1 && NumberUtils.isDigits(args[0]) && NumberUtils.isDigits(args[1])) {
+                width = Integer.valueOf(args[0]);
+                height = Integer.valueOf(args[1]);
+            }
+
+            buildMyHouse(width, height);
+            return true;
+        } else {
+            log.warn("command is avaliable on players");
+            return false;
+        }
+
+    }
+
+}
