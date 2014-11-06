0dba3d16579c98159dcd650f6020924fd693e157
toto castaldi <toto.castaldi@gmail.com>
2014-11-06 23:58:55 +0100

    exclude bukkit from build

--- src/main/java/it/toto/minecraft/plugin/command/NameCow.java
+++ src/main/java/it/toto/minecraft/plugin/command/NameCow.java
@@ -6,38 +6,33 @@ import com.google.inject.name.Named;
 import it.toto.minecraft.plugin.CommandExecution;
 import it.toto.minecraft.plugin.util.DebugLog;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.math.NumberUtils;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Cow;
 import org.bukkit.entity.Player;
-
-import java.util.Arrays;
 
 /**
  * Created by toto on 65/11/14.
  */
 @Slf4j
-public class PlayASound implements CommandExecution {
+public class NameCow implements CommandExecution {
 
 
     private final DebugLog debugLog;
-    private float volume = 0.1f;
-    private float pitch = 1.0f;
 
     @Inject
-    public PlayASound() {
+    public NameCow() {
         debugLog = DebugLog.of(log);
     }
 
     @Override
     public boolean go(CommandSender sender, Command command, Iterable<String> args) {
-        String soundName = Iterables.getFirst(args, Sound.GHAST_SCREAM.name());
         if (sender instanceof Player) {
-            Player me = (Player) sender;
-            me.playSound(me.getLocation(), Sound.valueOf(soundName), volume, pitch);
+            Player me = (Player)sender;
+            Cow cow = me.getWorld().spawn(me.getLocation(), Cow.class);
+            cow.setCustomName(Iterables.getFirst(args, "Cow"));
+            cow.setCustomNameVisible(true);
             return true;
         } else {
             log.warn("command is avaliable on players");
