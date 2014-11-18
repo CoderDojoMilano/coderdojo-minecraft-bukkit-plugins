61aacc2c3b979a92d4712d173113f4254413da67
antonio castaldi <toto.castaldi@gmail.com>
2014-11-18 12:35:45 +0100

    deploy checks

--- build-and-deploy-plugins.sh
+++ build-and-deploy-plugins.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+set -e
+
+# Check if there are one argument
+if [ $# -eq 1 ]; then
+   # Check if the input file actually exists.
+   if [ ! -d "$1" ]; then
+     echo "The server folder $1 does not exist... check them"
+     exit 1
+   fi
+else
+   echo "Usage: $0 [server folder]"
+   exit 1
+fi
+
+
 PLUGIN_FOLDER="$1/plugins"
 LOCATION=$(dirname "$(readlink -fn "$0")")  
 
