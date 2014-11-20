53b25394eb788d326268df7f8468e5808cb179bf
antonio castaldi <toto.castaldi@gmail.com>
2014-11-20 15:47:30 +0100

    lava vision

---
+++ toto-plugin/src/main/java/it/toto/minecraft/plugin/command/LavaVision.java
@@ -0,0 +1,57 @@
+package it.toto.minecraft.plugin.command;
+
+import com.google.common.collect.Iterables;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import it.toto.minecraft.plugin.util.DebugLog;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.NumberUtils;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Cow;
+import org.bukkit.entity.Player;
+import org.bukkit.util.BlockIterator;
+
+/**
+ * Created by toto on 05/11/14.
+ */
+@Slf4j
+public class LavaVision implements CommandExecution {
+
+
+    private final DebugLog debugLog;
+
+    @Inject
+    public LavaVision() {
+        debugLog = DebugLog.of(log);
+    }
+
+    @Override
+    public boolean go(CommandSender sender, Command command, Iterable<String> args) {
+        if (sender instanceof Player) {
+            Player me = (Player)sender;
+            BlockIterator sightItr = new BlockIterator (me, 100);
+            boolean found = false;
+            while (sightItr.hasNext() && !found) {
+                Block b = sightItr.next();
+                me.playEffect(b.getLocation(), Effect.MOBSPAWNER_FLAMES, null);
+                if (b.getType() != Material.AIR) {
+                    b.setType(Material.LAVA);
+                    me.playSound(b.getLocation(), Sound.EXPLODE, 1.0f, 0.5f);
+                    found = true;
+                }
+            }
+            return true;
+        } else {
+            log.warn("command is avaliable on players");
+            return false;
+        }
+
+    }
+
+}
