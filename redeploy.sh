46a17e9f3b10325631eaa58fafd7da12ab1838e1
toto castaldi <toto.castaldi@gmail.com>
2014-11-24 21:25:13 +0100

    server nuovo

--- redeploy.sh
+++ redeploy.sh
@@ -5,6 +5,6 @@ set -e
 export JAVA_HOME=/home/ubuntu/jdk1.8.0_25
 cd /home/ubuntu/coderdojo-minecraft-bukkit-plugins
 git pull
-./build-and-deploy-plugins.sh /home/ubuntu/crafbukkit
 /home/ubuntu/crafbukkit/server.sh stop
+./build-and-deploy-plugins.sh /home/ubuntu/crafbukkit
 /home/ubuntu/crafbukkit/server.sh start
