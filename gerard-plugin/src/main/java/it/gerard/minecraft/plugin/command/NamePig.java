f29a00e5d825556f2ded379ce0ae87767cb473c1
Gerard Pasquini <gherardo.pasquini@gmail.com>
2014-12-10 21:46:58 +0100

    nuovo plugin di gerard
    crea un "pig" e dagli un nome (come quello della mucca).

---
+++ gerard-plugin/src/main/java/it/gerard/minecraft/plugin/command/NamePig.java
@@ -0,0 +1,32 @@
+package it.toto.minecraft.plugin.command;
+
+import com.google.common.collect.Iterables;
+import lombok.extern.slf4j.Slf4j;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by gerard on 08/12/14.
+ */
+@Slf4j
+public class NamePig implements CommandExecution {
+
+
+    @Override
+    public boolean go(CommandSender sender, Command command, Iterable<String> args) {
+        if (sender instanceof Player) {
+            Player me = (Player)sender;
+            Pig pig = me.getWorld().spawn(me.getLocation(), Pig.class);
+            pig.setCustomName(Iterables.getFirst(args, "Pig"));
+            pig.setCustomNameVisible(true);
+            return true;
+        } else {
+            log.warn("command is avaliable on players");
+            return false;
+        }
+
+    }
+
+}
